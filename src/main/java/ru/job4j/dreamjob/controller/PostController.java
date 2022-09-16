fe400d0c2b5f15c836e795f84a62454e5f03ad9e
Arrcady555 <parfenov7233@gmail.com>
2022-09-16 20:51:13 +0300

    HTML form. Создание вакансии. [#504844]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.job4j.dreamjob.store.PostStore;
+import ru.job4j.dreamjob.model.Post;
 
 @Controller
 public class PostController {
@@ -14,5 +15,11 @@ public class PostController {
     public String posts(Model model) {
         model.addAttribute("posts", store.findAll());
         return "posts";
+    }
+
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post(0, "Заполните поле"));
+        return "addPost";
     }
 }
