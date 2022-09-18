a94001642e44def1ffe85f463fc8603e9a8d519f
Arrcady555 <parfenov7233@gmail.com>
2022-09-18 17:17:25 +0300

    PostController.createPost. Создание вакансии. [#504849 #344680]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,25 +2,18 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PostStore {
 
     private static final PostStore INST = new PostStore();
-
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Post> newPosts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "no bad job!",
-                new Date(2022, Calendar.SEPTEMBER, 10)));
-        posts.put(2, new Post(2, "Middle Java Job", "good job!",
-                new Date(2022, Calendar.SEPTEMBER, 11)));
-        posts.put(3, new Post(3, "Senior Java Job", "excellent job!",
-                new Date(2022, Calendar.SEPTEMBER, 1)));
+        posts.putAll(this.newPosts);
     }
 
     public static PostStore instOf() {
@@ -29,5 +22,9 @@ public class PostStore {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public void add(Post post) {
+        newPosts.put(post.getId(), post);
     }
 }
